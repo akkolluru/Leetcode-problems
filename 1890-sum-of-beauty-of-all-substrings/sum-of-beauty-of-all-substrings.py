2331094b30a7ada25530700b32a2bd8fbf71eafa
akkolluru <148373233+akkolluru@users.noreply.github.com>
2025-10-11 21:14:34 +0530

    Time: 1373 ms (67.71%) | Memory: 17.9 MB (17.03%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def beautySum(self, s: str) -> int:
+        total = 0
+        for i in range(len(s)):
+            freq = Counter()
+            for j in range(i, len(s)):
+                freq[s[j]] += 1
+                total += max(freq.values()) - min(freq.values())
+        return total
+        
